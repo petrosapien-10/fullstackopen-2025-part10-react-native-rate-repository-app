f01e0ea049f43b2f25b6dbaaf14a782077ade6ed
petrosapien-10 <nhutlongnguyen.work@gmail.com>
2025-09-08 22:43:36 +0300

    completed part10-3

--- App.jsx
+++ App.jsx
@@ -1,22 +1,7 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import Main from "./src/components/Main";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>THIS IS NEW</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+  return <Main />;
+};
 
-const styles = StyleSheet.create({
-  // eslint-disable-next-line react-native/no-color-literals
-  container: {
-    alignItems: "center",
-    backgroundColor: "#ffffff",
-    flex: 1,
-    justifyContent: "center",
-  },
-});
+export default App;
