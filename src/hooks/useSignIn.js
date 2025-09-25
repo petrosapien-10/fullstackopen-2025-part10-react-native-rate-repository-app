fcfb45ec8abe01ab36d76c6f324da9a9037d70db
petrosapien-10 <nhutlongnguyen.work@gmail.com>
2025-09-26 01:26:21 +0300

    completed ex 10.13

---
+++ src/hooks/useSignIn.js
@@ -0,0 +1,19 @@
+import { AUTHENTICATE } from "../graphql/mutations";
+import { useMutation } from "@apollo/client";
+
+const useSignIn = () => {
+  const [mutate, result] = useMutation(AUTHENTICATE);
+
+  const signIn = async ({ username, password }) => {
+    return mutate({
+      variables: {
+        username,
+        password,
+      },
+    });
+  };
+
+  return [signIn, result];
+};
+
+export default useSignIn;
